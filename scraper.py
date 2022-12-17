cd6be3ba7d373f6a1999bda07748a671abcb8612
LucaBarban <45258881+LucaBarban@users.noreply.github.com>
2022-12-17 10:27:40 +0100

    Adding support to multiple sections

--- scraper.py
+++ scraper.py
@@ -12,24 +12,38 @@ from obj_lesson import Lezione
 def scrapeLessons(url, ignore) -> list[Lezione]:
     response = requests.get(url).text
     response = response[response.find(
-        "<div class=\"card-body\">"):response.find("<!-- fine col 9-->")]
-    response = "<bs>"+response[23:-300]+"</bs>"
+        "<div class=\"tab-content\">"):response.find("<!-- fine col 9-->")]
+    response = response[0:-7]
 
-    # some debug stuff
-    # """
+
     # with open('testpagina.html', 'w') as file:
     #     file.write(response)
-    # """
-    #
+
+
+    sections = []
+
+    for i in range(1, 5):
+        tmpStart = "<div class=\"card tit-no-border espansione\" id=\"periodo-" + str(i) + "\">"
+        tmpFinish = "<div class=\"card tit-no-border espansione\" id=\"periodo-" + str(i + 1) + "\">"
+        if tmpStart in response:
+            tmpSection = response[response.find(tmpStart):response.find(tmpFinish)]
+            if i == 1:
+                sections.append("<bs>" + tmpSection[tmpSection.find("<div class=\"card-body\">")+23:-259] + "</bs>")
+            else:
+                sections.append("<bs>" + tmpSection[tmpSection.find("<div class=\"card-body\">")+23:-282] + "</bs>")
+
+            # with open('testpagina.html', 'w') as file:
+            #     if i == 1:
+            #         file.write("<bs>" + tmpSection[tmpSection.find("<div class=\"card-body\">")+23:-259] + "</bs>")
+            #     else:
+            #         file.write("<bs>" + tmpSection[tmpSection.find("<div class=\"card-body\">")+23:-282] + "</bs>")
+    
     # """
     # with open('testpagina.txt', 'w') as file:
     #     for lezione in oraribetter:
     #         print(lezione)
     #         file.write(str(lezione) + ";\n")
     # """
-
-    tree = ET.ElementTree(ET.fromstring(response))
-    # ET.dump(tree)
 
     orari = []
     subject = ""
@@ -41,58 +55,72 @@ def scrapeLessons(url, ignore) -> list[Lezione]:
     classes = ""
     time = ""
 
-    root = tree.getroot()
+    print("Found", len(sections), "sections on", url)
 
-    for thing in root:
-        if thing.tag == "h5":
-            subject = list(thing)[0].text
-            subject = str(subject).strip()
-        elif thing.tag == "div":
-            if subject in ignore:
-                continue
-            children = list(thing)
-            children1 = list(children)[0]
-            children2 = list(children)[1]
+    for section in sections:
+        tree = ET.ElementTree(ET.fromstring(section))
+        #ET.dump(tree)
 
-            children1 = list(children1)[0]
+        root = tree.getroot()
 
-            day = list(children1)[1].text
-            tmp = list(children1)[1]
-            tmporario = str(tmp[0].text).split(" - ")
-            time = tmporario[0] + "-" + tmporario[1]
-            tmp = list(children1)[2]
-            annotations = tmp[0].text
-            tmp = list(children1)[3]
-            classes = tmp[0].text
-            location = list(children1)[4].text
-            children = list(children1)[1].text
+        for thing in root:
+            if thing.tag == "h5":
+                subject = list(thing)[0].text
+                subject = str(subject).strip()
+            elif thing.tag == "div":
+                if subject in ignore:
+                    continue
+                children = list(thing)
+                children1 = list(children)[0]
+                children2 = list(children)[1]
 
-            # print(subject + ", " + day + ", " + time + ", " + classes + ", " + location)
+                children1 = list(children1)[0]
 
-            children2 = list(children2)[0]
-            children2 = list(children2)[0]
-            children2 = list(children2)[0]
-            tbody = list(children2)[1]
-            for entry in tbody:
-                # print(entry.attrib)
-                tmp = list(entry)
-                date = tmp[0].text
-                # print("annotazione:", annotazioni)
-                attivita = annotations.title() if annotations is not None else tmp[1].text
-                # print(attivita)
-                # print(subject)
-                prof = tmp[2].text.title()
-                if len(list(tmp[2])) > 0: #stupid case where things are writte into an em tag
-                    #print(list(tmp[2])[0].text.title())
-                    orari.append(Lezione(subject, day, date, list(tmp[2])[0].text.title(),
-                                prof, location, classes, time, 0))
-                else:
-                    orari.append(Lezione(subject, day, date, attivita,
-                                prof, location, classes, time, 0))
-                # print(orari, "\nl")
-        else:
-            print("rip")
-            exit()
+                day = list(children1)[1].text
+                tmp = list(children1)[1]
+                tmporario = str(tmp[0].text).split(" - ")
+                time = tmporario[0] + "-" + tmporario[1]
+                tmp = list(children1)[2]
+                annotations = tmp[0].text
+                tmp = list(children1)[3]
+                classes = tmp[0].text
+                location = list(children1)[4].text
+                if (location is None):
+                    location = ""
+                children = list(children1)[1].text
+
+                # print(subject + ", " + day + ", " + time + ", " + classes + ", " + location)
+
+                children2 = list(children2)[0]
+                children2 = list(children2)[0]
+                children2 = list(children2)[0]
+                tbody = list(children2)[1]
+                for entry in tbody:
+                    # print(entry.attrib)
+                    tmp = list(entry)
+                    date = tmp[0].text
+                    # print("annotazione:", annotazioni)
+                    attivita = annotations.title() if annotations is not None else tmp[1].text
+                    # print(attivita)
+                    # print(subject)
+                    prof = tmp[2].text.title()
+                    if len(list(tmp[2])) > 0: #stupid case where things are writte into an em tag
+                        #print(list(tmp[2])[0].text.title())
+                        try:
+                            orari.append(Lezione(subject, day, date, list(tmp[2])[0].text.title(),
+                                        prof, location, classes, time, 0))
+                        except Exception as e:
+                            print("Died scraping:", e)
+                            print(subject, day, date, tmp[2].text.title(), prof, location, classes, time, 0)
+
+
+                    else:
+                        orari.append(Lezione(subject, day, date, attivita,
+                                    prof, location, classes, time, 0))
+                    # print(orari, "\nl")
+            else:
+                print("rip")
+                exit()
 
     # sort the lessons (useful for the next step)
     orari.sort(key=lambda Lezione: datetime.strptime(
